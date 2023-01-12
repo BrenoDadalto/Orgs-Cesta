ee85ea2bdb7a2527872666f1feff7d636053cb29
Breno Dadalto <brenodadalto@hotmail.com>
2023-01-12 16:45:35 -0300

    My first React Native App

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useCallback } from 'react';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat'
+import * as SplashScreen from 'expo-splash-screen';
+
+export default function App() {
+
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fonteCarregada) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fonteCarregada]);
+
+  if (!fonteCarregada) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <StatusBar />
+      <Cesta {...mock} />
+    </SafeAreaView>
+  );
+}
